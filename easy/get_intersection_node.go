8efd55e158983cd0b50c7052e587d607e0269e44
vamoscb <965090342@qq.com>
2020-07-02 22:51:39 +0800

    新增代码

---
+++ easy/get_intersection_node.go
@@ -0,0 +1,33 @@
+package easy
+
+/*
+	leetcode 160题
+
+	编写一个程序，找到两个单链表相交的起始节点。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+//GetIntersectionNode ...
+func GetIntersectionNode(headA, headB *ListNode) *ListNode {
+	curA, curB := headA, headB
+	for curA != curB {
+		if curA == nil { // 如果第一次遍历到链表尾部，就指向另一个链表的头部，继续遍历，这样会抵消长度差。如果没有相交，因为遍历长度相等，最后会是 nil ==  nil
+			curA = headB
+		} else {
+			curA = curA.Next
+		}
+		if curB == nil {
+			curB = headA
+		} else {
+			curB = curB.Next
+		}
+	}
+	return curA
+}
